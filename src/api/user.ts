5c1d545851202da5ea779be3b80b34239a935ed5
Zohaib-CodeUpScale <zohaib.afzal@codeupscale.com>
2025-08-06 15:11:05 +0500

    all roles profile data prefil and update api integrate also redux update and password api integrated

---
+++ src/api/user.ts
@@ -0,0 +1,37 @@
+import BaseApi from "./baseapi";
+
+class UserApi extends BaseApi {
+  baseUrl: string = "user";
+  constructor() {
+    super();
+  }
+
+  async userUpdate(
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    accountType: string,
+    phoneNo: string,
+    status: string,
+    companyName: string,
+    bio: string,
+    address: string
+  ) {
+    const data = await this.patch(`${this.baseUrl}/${id}`, {
+      firstName,
+      lastName,
+      email,
+      accountType,
+      status,
+      phoneNo,
+      companyName,
+      bio,
+      address,
+    });
+    return data;
+  }
+}
+
+export const userApi = new UserApi();
+export default userApi;
